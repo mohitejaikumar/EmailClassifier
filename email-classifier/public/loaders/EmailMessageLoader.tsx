5d266ea14cbbb3ac6a5cc5b62f160fcc08680405
mohitejaikumar <mohitjaikumar@gmail.com>
2024-06-08 22:39:53 +0530

    added loader

--- email-classifier/public/loaders/EmailMessageLoader.tsx
+++ email-classifier/public/loaders/EmailMessageLoader.tsx
@@ -3,19 +3,18 @@ export const EmailMessageLoader = () => {
     <>
       {Array.from({ length: 3 }).map((item, index) => {
         return (
-          <div
-            key={index}
-            className="rounded-lg text-card-foreground shadow-sm bg-gray-800 border border-white">
+          <div className="rounded-lg text-card-foreground shadow-sm bg-gray-800 border border-white animate-pulse">
             <div className="flex-col space-y-1.5 p-6 flex justify-between">
               <div className="flex justify-between items-center">
+                <div className="h-6 bg-white w-1/2 rounded"></div>
                 <div className="h-6 bg-white w-1/2 rounded"></div>
               </div>
             </div>
             <div className="p-6 pt-0">
-              <div className="h-4 bg-slate-300 w-full rounded"></div>
+              <div className="h-4 bg-white w-full rounded"></div>
             </div>
             <div className="p-6 pt-0">
-              <div className="h-4 bg-gray-200 w-full rounded"></div>
+              <div className="h-4 bg-white w-full rounded"></div>
             </div>
           </div>
         );
